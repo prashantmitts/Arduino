59e42d35ade6a3a81db532fbdf595db38b46752e
Prashant Mittal <Prashant Mittal>
2016-06-06 01:35:54 +0530

    simple LED

---
+++ LED-13_blink/LED-13_blink.ino
@@ -0,0 +1,16 @@
+//LED
+
+int pin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(pin,HIGH);
+  delay(1000);
+  digitalWrite(pin,LOW);
+  delay(1000);
+}
